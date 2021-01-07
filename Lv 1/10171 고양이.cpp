cfc83c3c266b6914abcf091a5e888e648078e73d
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-01-07 14:44:52 +0900

    입출력과 사칙연산 1단계

---
+++ Lv 1/10171 고양이.cpp
@@ -0,0 +1,16 @@
+﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+int main() {
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+}
---
+++ Lv 1/10718 We love kriii.cpp
@@ -0,0 +1,14 @@
+﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <iostream>
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+int main() {
+	for(int i = 0; i<2;i++)
+		cout << "강한친구 대한육군" << endl;
+}
