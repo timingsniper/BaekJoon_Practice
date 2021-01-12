a70dd93d9dc7721f7fc058da24cc551f4b991894
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-01-13 01:51:23 +0900

    Add files via upload

---
+++ Lv 2/14681 사분면 고르기.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream> 
+#include <cstring> 
+#include <cstdlib> 
+#include <cstdio> 
+using namespace std;
+
+int main() {
+	int x, y;
+	char b;
+	cin >> x;
+	cin >> y;
+
+	if (x > 0) {
+		if (y > 0) {
+			cout << "1";
+			return 0;
+		}
+		else {
+			cout << "4";
+			return 0;
+		}
+
+	}
+	else {
+		if (y > 0) {
+			cout << "2";
+			return 0;
+		}
+		else {
+			cout << "3";
+			return 0;
+		}
+	}
+	
+}
