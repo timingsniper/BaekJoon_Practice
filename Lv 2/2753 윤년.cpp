f17a9eb020f42d06e3a3815927e1c1d79ad8344f
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-01-13 01:43:35 +0900

    Add files via upload

---
+++ Lv 2/2753 윤년.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+#include <cstring> 
+#include <cstdlib> 
+#include <cstdio> 
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)) {
+		cout << "1";
+	}
+	else
+		cout << "0";
+}
