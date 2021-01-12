ff397068d72005135b4333beccd4ccd24bb36570
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-01-13 01:40:41 +0900

    Add files via upload

---
+++ Lv 2/9498 시험 성적.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+#include <cstring> 
+#include <cstdlib> 
+#include <cstdio> 
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+
+	if (a >= 90 && a <= 100) {
+		cout << "A";
+	}
+	else if (a >= 80 && a <= 89) {
+		cout << "B";
+	}
+	else if (a >= 70 && a <= 79) {
+		cout << "C";
+	}
+	else if (a >= 60 && a <= 69) {
+		cout << "D";
+	}
+	else
+		cout << "F";
+}
