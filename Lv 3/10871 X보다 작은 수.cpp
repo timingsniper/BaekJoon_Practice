022e6f55f57154ae168bf2d62a19f07e49263f3a
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-01-13 02:35:31 +0900

    Add files via upload

---
+++ Lv 3/10871 X보다 작은 수.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream> 
+#include <cstring> 
+#include <cstdlib> 
+#include <cstdio> 
+using namespace std;
+
+int main() {
+	//속도up
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int n, x, element;
+	char spacing;
+	cin >> n >> x;
+
+	for (int i = 0; i < n; i++) {
+		cin >> element;
+		if (element < x) {
+			cout << element << " ";
+		}
+	}
+}
