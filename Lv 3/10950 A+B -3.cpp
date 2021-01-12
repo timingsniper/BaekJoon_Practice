82293406d0a64be77203f51808e4ac0f76dff699
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-01-13 02:03:21 +0900

    Add files via upload

---
+++ Lv 3/10950 A+B -3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+#include <cstring> 
+#include <cstdlib> 
+#include <cstdio> 
+using namespace std;
+
+int main() {
+	int n, a, b;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+}
