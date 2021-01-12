78ef8ec06dd6d97dbfa95be6cb34aaa6b7e4e429
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-01-13 02:18:00 +0900

    Add files via upload

---
+++ Lv 3/2438 별 찍기 - 1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+#include <cstring> 
+#include <cstdlib> 
+#include <cstdio> 
+using namespace std;
+
+int main() {
+	//속도up
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		int cop = i;
+		while (cop != 0) {
+			cout << "*";
+			cop--;
+		}
+		cout << "\n";
+	}
+
+
+}
