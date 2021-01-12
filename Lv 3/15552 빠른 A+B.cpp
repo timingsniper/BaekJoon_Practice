4762ae5da0ad7ef9c6edd5f1471977326e218fc4
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2021-01-13 02:09:05 +0900

    Add files via upload

---
+++ Lv 3/15552 빠른 A+B.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> 
+#include <cstring> 
+#include <cstdlib> 
+#include <cstdio> 
+using namespace std;
+
+int main() {
+	//속도up
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int n, a, b;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+
+
+}
